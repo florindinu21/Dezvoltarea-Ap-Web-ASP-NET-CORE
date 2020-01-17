50ef0b005cceb9d8e6a4adbe62ab09c95ef63cb0
nxf53310 <florin.dinu@nxp.com>
2020-01-17 20:48:34 +0200

    Renamed the project directories because the VS is using already those names in paths

---
+++ HomeworkC--master/Models/AppContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Models
+{
+    public class AppContext : IdentityDbContext<AppUser>
+    {
+
+        public DbSet<Flight> Flight { get; set; }
+
+        public DbSet<Booking> Booking { get; set; }
+       /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=hotel;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }*/
+        public AppContext(DbContextOptions options) : base(options)
+        { }
+        public AppContext()
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Booking>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.HasOne(x => x.AppUser)
+                    .WithMany(x => x.bookings)
+                    .HasForeignKey(x => x.AppUserID)
+                    ;
+                entity.HasOne(x => x.Flight)
+                    .WithMany(x => x.bookings)
+                    .HasForeignKey(x => x.FlightId);
+                
+            });
+
+
+            modelBuilder.Entity<Flight>(entity => {
+                entity.HasKey(x => x.Id);
+
+               
+            });
+
+            modelBuilder.Entity<AppUser>()
+                .Property(e => e.Id)
+                .ValueGeneratedOnAdd();
+
+        }
+    }
+}
---
+++ HomeworkC--master/Repositories/IRepositoryWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Repositories
+{
+    public interface IRepositoryWrapper
+    {
+        IFlightRepository Flight { get; }
+        IBookingRepository Booking { get; }
+        void Save();
+    }
+}
